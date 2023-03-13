eb61d3a85909c83eea4a664a6ed4bf6080dd42d1
mosekyle <gitaumoseskiarie@gmail.com>
2023-03-13 11:55:04 -0700

    Intro to Javascript

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,10 +1,5 @@
 #!/usr/bin/node
-/*
-   A JS Script that prints the elements of an array in a single line.
-   Author: Moses Gitau
-   Using for loop to get this done, however other loop can still work.
-*/
-const lang = ['c is fun', 'Python is cool', 'JavaSript is amazing'];
-for (const item of lang) {
-  console.log(item);
+const strings = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (const string of strings) {
+  console.log(string);
 }
